7f034602a7f34606f921f22aa1be1b264a841bd5
Takumikimura141899 <kimura141899@gmail.com>
2021-07-13 21:24:04 +0900

    Organisms/VideosListItem 動画リストアイテムまで作成

---
+++ src/components/organisms/VideosListItem/index.stories.jsx
@@ -0,0 +1,6 @@
+import VideoListItem from '.';
+import video from './sampleData.json';
+
+export default { title: 'organisms/VideoListItem' };
+
+export const videoListItem = () => <VideoListItem video={video} />;
